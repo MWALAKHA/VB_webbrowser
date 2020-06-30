411a7220a5d82d6bcb0076c7e2e7ed88e83a536c
GIBSON MWALAKHA <mwalakha2020@outlook.com>
2020-06-30 13:10:12 +0300

    Update Form1.vb

--- webbrowser/Form1.vb
+++ webbrowser/Form1.vb
@@ -12,7 +12,7 @@
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        WebBrowser.Navigate("www.mwalimusite.wordpress.com")
+        WebBrowser.Navigate("https://mwalimusite.wordpress.com/")
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
